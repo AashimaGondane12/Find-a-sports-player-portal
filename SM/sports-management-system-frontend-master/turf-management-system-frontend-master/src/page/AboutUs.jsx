4e48d3d695655b01c84bb644b42ec16e93e53fd7
aashima <aashimagondane74@gmail.com>
2024-02-28 15:17:27 +0530

    adding files

---
+++ SM/sports-management-system-frontend-master/turf-management-system-frontend-master/src/page/AboutUs.jsx
@@ -0,0 +1,15 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b><p>
+      Sports Management System is your go-to platform for sports interested people's, athletes across various sports disciplines! Whether you're organizing a game, looking to book grounds, turfs and courts, or simply seeking like-minded individuals to engage in sporting activities, our user-friendly interface makes it easy to find the perfect match. From soccer to basketball, tennis to rugby, discover  and book turfs and courts of all skill levels in your area.
+      </p>
+      </b>
+
+    </div>
+  );
+};
+
+export default AboutUs;
